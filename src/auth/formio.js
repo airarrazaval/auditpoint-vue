93a486535beda62879e22bac405675ccc4813243
Unknown <a.irarrazaval.lasen+github@gmail.com>
2018-03-11 22:16:07 +0100

    optimized auth with formiojs libraries

--- src/auth/formio.js
+++ src/auth/formio.js
@@ -1,19 +1,59 @@
-import router from '../router'
+import router from '@/router'
 import Formio from 'formiojs'
 
-// URL and endpoint constants
-const API_URL = 'http://localhost:3001/'
-const LOGIN_URL = API_URL + 'user/login'
-// const SIGNUP_URL = API_URL + 'user/register'
+const API_URL = 'http://localhost:3001'
 
+Formio.setBaseUrl(API_URL)
+
+export function isAuthenticated () {
+  return !!Formio.getUser() && !!Formio.getToken()
+}
+
+export function setToken (token) {
+  return Formio.setToken(token)
+}
+
+export function getToken () {
+  return Formio.getToken()
+}
+
+export function setUser (user) {
+  return Formio.setUser(user)
+}
+
+export function getUser () {
+  return Formio.getUser()
+}
+
+export function login (context, credentials, redirect) {
+  console.log('credentials', credentials)
+  console.log('redirect', redirect)
+  return Formio.makeStaticRequest(API_URL + '/user/login', 'post', {data: credentials}).then((response) => {
+    console.log('login successfull')
+    Formio.setUser(response)
+
+    // Redirect to a specified route
+    if (redirect) {
+      router.push(redirect)
+    }
+  }).catch((err) => {
+    context.error.show = true
+    context.error.message = err.details ? err.details[0].message : err.message || err
+  })
+}
+
+export function logout () {
+  Formio.logout()
+  router.push({name: 'Login'})
+}
+
+/*
 export default {
-  formio: null,
   // Send a request to the login URL and save the returned JWT
   doLogin (context, credentials, redirect) {
     if (!credentials.password) {
       context.error.show = true
       context.error.message.push('empty password')
-
     } else {
       let formio = new Formio(LOGIN_URL)
       formio.saveSubmission({
@@ -53,3 +93,4 @@ export default {
     })
   }
 }
+*/
